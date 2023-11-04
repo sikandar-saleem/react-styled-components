49201788bc494baf0ed209e8cdd14561eb5219bd
Sikandar Saleem <sikander.saleem@camp1.tkxel.com>
2023-11-04 10:09:51 +0500

    Create: Animations/Keyframes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { AnimatedReactLogo } from "./components/Animation/Logo.styles";
 import Button, {
   GradientButton,
   SubmitButton,
 } from "./components/Button/Button";
 
+import "./app.css";
+
+import logo from "./assets/icon.png";
+
 function App() {
   return (
     <>
+      <AnimatedReactLogo src={logo} />
+      <br />
+      <br />
       <Button>Button</Button>
       <Button variant="outline">Button</Button>
       <GradientButton as="a">Button</GradientButton>
